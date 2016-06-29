09d578563db24b4540fa4efa49b0230d88e7dd03
spartacus <saeidzarein95@gmail.com>
2016-06-29 04:43:43 -0700

    Updated !!

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -8,7 +8,7 @@ local f = assert(io.popen('/usr/bin/git describe --tags', 'r'))
 VERSION = assert(f:read('*a'))
 f:close()
 
--- This function is called when tg receive a msg
+-- This function is called when tg receive a msgs
 function on_msg_receive (msg)
   if not started then
     return
